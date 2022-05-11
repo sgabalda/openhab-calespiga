7c1d742d49907914bb319f5255f085157948adfc
s.gabalda <s.gabalda@stuart.com>
2022-05-11 22:29:49 +0200

    remove debug info

--- shell-scripts/gpio-output.py
+++ shell-scripts/gpio-output.py
@@ -4,14 +4,6 @@ import sys
  
 total = len(sys.argv)
 
-cmdargs = str(sys.argv)
-print ("The total numbers of args passed to the script: %d " % total)
-print ("Args list: %s " % cmdargs)
-# Pharsing args one by one 
-print ("Script name: %s" % str(sys.argv[0]))
-print ("First argument: %s" % str(sys.argv[1]))
-print ("Second argument: %s" % str(sys.argv[2]))
-
 def getGPIONum():
     try:
         return int(sys.argv[1])
