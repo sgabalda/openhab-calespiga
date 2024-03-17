097a579290b78514ad210f3b010c0251c5fa0c80
s.gabalda <s.gabalda@stuart.com>
2024-03-17 21:31:39 +0100

    add memory to arduinos and change updated at

--- arduino/calentador-uno/calentador-uno.ino
+++ arduino/calentador-uno/calentador-uno.ino
@@ -1,16 +1,14 @@
 /*
   Turns on the three relays for the heater
 */
-#include <UIPEthernet.h>
+#include <SPI.h>
+#include <Ethernet.h>
 #include <PubSubClient.h>
-#include <MemoryFree.h>
 
 #define ARDUINO_CLIENT_ID "arduino_calentador"                     // Client ID for Arduino pub/sub
 #define SUB_CALENTADOR "arduino_calentador/potencia/set"
 #define PUB_CALENTADOR "arduino_calentador/potencia/status"
 #define PUB_TERMOSTAT "arduino_calentador/termostat/status"
-#define PUB_TERMOSTAT "arduino_calentador/termostat/status"
-#define TOPIC_MEMORY "arduino_calentador/memory"
 #define MAX_TIME_BETWEEN_ORDERS 3*60*1000   // 3 minutes
 
 // Networking details
@@ -45,7 +43,7 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println(F("Calentador Arduino starting")); 
+  Serial.println("Calentador Arduino starting"); 
   
   
   // MTTQ parameters
@@ -57,33 +55,23 @@ void setup() {
 
 // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println(F("Ethernet shield not found, can't run."));
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
     while (true) {
       delay(1); // do nothing, no point running without Ethernet hardware
     }
   }
   if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println(F("Ethernet cable not connected."));
+    Serial.println("Ethernet cable is not connected.");
   }
  
   delay(1500); // Allow hardware to stabilize 1.5 sec
 
-  Serial.print(F("IP is at "));
+  Serial.print("IP is at ");
   Serial.println(Ethernet.localIP());
 
   lastOrder = millis();
   
-}
 
-void reportMemory(){
-  int freeMem = freeMemory();
-
-  char cstr[16];
-  itoa(freeMem, cstr, 10);
-  client.publish(TOPIC_MEMORY,cstr);
-
-  Serial.print(F("Memory free (bytes): "));
-  Serial.println(freeMem);
 }
 
 void loop() {
@@ -93,27 +81,30 @@ void loop() {
   client.loop();
   checkMaxTime();
   checkOptStatus();
-  reportMemory();
   delay(2000);
 }
 
 void checkOptStatus(){
-  Serial.println(F("Checking optocuopler"));
+  Serial.println("Checking optocuopler");
   byte optStatus = digitalRead(OPTO_READ);
   if(optStatus == optoHOT){
-    Serial.println(F("HOT, sending status"));
+    Serial.println("HEATER is HOT, sending status");
     client.publish(PUB_TERMOSTAT, "HOT");
   }else{
-    Serial.println(F("COLD, sending status"));
+    Serial.println("HEATER is COLD, sending status");
     client.publish(PUB_TERMOSTAT, "COLD");
   }
 }
 
 void checkMaxTime(){
-  Serial.print(F("ms since last order: "));
-  Serial.println(millis()-lastOrder);
+  Serial.println(
+      String("Time since last oreder is: ")
+      +String(millis()-lastOrder) + String("ms"));
   if(millis()-lastOrder > MAX_TIME_BETWEEN_ORDERS){
-    Serial.println(F("Too much time between orders, turning off calentador:"));
+    Serial.println(
+      String("Too much time between orders, turning off calentador:")
+      +String(millis()-lastOrder) + String("ms")
+    );
     calentadorOff();
     lastOrder=millis();
   }
@@ -124,23 +115,23 @@ void reconnect()
 
   // Loop until reconnected
   while (!client.connected()) {
-    Serial.print(F("Attempting MQTT connection ... "));
+    Serial.print("Attempting MQTT connection ... ");
     // Attempt to connect
     if (client.connect(ARDUINO_CLIENT_ID)) {
-      Serial.println(F("connected"));
+      Serial.println("connected");
       // (re)subscribe
       client.subscribe(SUB_CALENTADOR);
     } else {
-      Serial.print(F("Connection failed, state: "));
+      Serial.print("Connection failed, state: ");
       Serial.print(client.state());
-      Serial.println(F(", retrying in 5 seconds"));
+      Serial.println(", retrying in 5 seconds");
       checkMaxTime();
       checkOptStatus();
       delay(5000); // Wait 5 seconds before retrying
     }
-    Serial.println(F("done 1st try to connect"));
+    Serial.println("done 1st try to connect");
   }
-  Serial.println(F("Client connected"));
+  Serial.println("Client connected");
 }
 
 // sub callback function
@@ -164,7 +155,7 @@ void callback(char* topic, byte* payload, unsigned int length)
   }else if (strcmp(message, "2000") == 0){
       calentador2000W();
   }else{
-      Serial.println(F("Error, message not valid:"));
+      Serial.println("Error, message not valid:");
       calentadorOff();
   }
 }
@@ -175,7 +166,7 @@ void calentadorOff(){
   turnRelay(RELAY_2, relayOFF);
   turnRelay(RELAY_1, relayOFF);
   client.publish(PUB_CALENTADOR, "OFF");
-  Serial.println(F("Calentador: Off"));
+  Serial.println("Calentador: Off");
 }
 
 void calentador500W(){
@@ -183,7 +174,7 @@ void calentador500W(){
   turnRelay(RELAY_2, relayOFF);
   turnRelay(RELAY_3, relayOFF);
   client.publish(PUB_CALENTADOR, "500");
-  Serial.println(F("Calentador: 500"));
+  Serial.println("Calentador: 500");
 }
 
 void calentador1000W(){
@@ -191,7 +182,7 @@ void calentador1000W(){
   turnRelay(RELAY_2, relayON);
   turnRelay(RELAY_3, relayOFF);
   client.publish(PUB_CALENTADOR, "1000");
-  Serial.println(F("Calentador: 1000"));
+  Serial.println("Calentador: 1000");
 }
 
 void calentador2000W(){
@@ -199,7 +190,7 @@ void calentador2000W(){
   turnRelay(RELAY_2, relayON);
   turnRelay(RELAY_3, relayON);
   client.publish(PUB_CALENTADOR, "2000");
-  Serial.println(F("Calentador: 2000"));
+  Serial.println("Calentador: 2000");
 }
 
 void turnRelay(int relay, int status) {
